36e5c29a5790125c859e9b586b406896fbd67bcc
Hoorad <hourrad.f@gmail.com>
2024-06-19 14:06:16 +1200

    Edited fabric.mod.json

--- src/main/java/net/minecobbraft/Minecobbraft.java
+++ src/main/java/net/minecobbraft/Minecobbraft.java
@@ -1,11 +1,13 @@
 package net.minecobbraft;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.impl.FabricLoaderImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Minecobbraft implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "minecobbraft";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
